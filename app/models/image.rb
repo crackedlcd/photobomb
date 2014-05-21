6a7564da5a9ffa9b40ff20a002db1e71a2793f3a
crackedlcd <crackedlcdscreen@gmail.com>
2014-05-21 03:17:06 -0400

    Delete Test::Unit test folder. Add hashing to Image filenames

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,9 @@
 class Image < ActiveRecord::Base
 	belongs_to :user
-	has_attached_file :content, :styles => { :thumb => "300x300>" }, :default_url => "/images/missing.jpg"
+	has_attached_file :content,
+	 :styles => { :thumb => "300x300>" }, :default_url => "/images/missing.jpg", 
+	 :url => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:hash.:extension", 
+	 :hash_secret => "d41d8cd98f00b204e9800998ecf8427e"
 	validates_attachment_content_type :content, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	acts_as_taggable
 end

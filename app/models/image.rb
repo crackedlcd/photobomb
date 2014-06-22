ffad66743d46d432e10ae276148d5af1384f46a0
crackedlcd <crackedlcdscreen@gmail.com>
2014-06-22 00:32:31 -0400

    Add bootstrap stylings to image view

--- app/models/image.rb
+++ app/models/image.rb
@@ -6,4 +6,6 @@ class Image < ActiveRecord::Base
 	 :hash_secret => "d41d8cd98f00b204e9800998ecf8427e"
 	validates_attachment_content_type :content, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 	acts_as_taggable
+
+	validates :content, presence: { message: "Please submit a valid image"}
 end

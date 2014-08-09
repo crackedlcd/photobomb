bed3e27446c2358f78f93c94b0b24b7769e4bfbf
crackedlcd <crackedlcdscreen@gmail.com>
2014-08-08 22:13:50 -0400

    add user_id column to image.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140519074912) do
+ActiveRecord::Schema.define(version: 20140809020456) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140519074912) do
     t.datetime "content_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "taggings", force: true do |t|
